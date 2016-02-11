28c5c8062ec757635fd3628595675374a05f415c
karas76 <karasev712@mail.ru>
2016-02-11 16:53:58 +0300

    Update WorkWithTriangles.java

--- src/main/java/WorkWithTriangles.java
+++ src/main/java/WorkWithTriangles.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * Created by Roman on 15.12.2015.
  */
-/** Сортирует треугольники по площади*/
+/** РЎРѕСЂС‚РёСЂСѓРµС‚ С‚СЂРµСѓРіРѕР»СЊРЅРёРєРё РїРѕ РїР»РѕС‰Р°РґРё*/
 public class WorkWithTriangles {
     public static void sortTriangles(List<Triangle> triangles) {
        Collections.sort(triangles, (Triangle a, Triangle b) -> a.getSquare().compareTo(b.getSquare()));
