91a335d262139de17a1930e29c30b72d2f8ec2b6
karas76 <karasev712@mail.ru>
2016-02-29 19:57:20 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src.txt

--- src/test/java/ParserTests.java
+++ src/test/java/ParserTests.java
@@ -35,7 +35,7 @@ public class ParserTests {
     }*/
     @Test
     public void TestGetSumTr() {
-        int ex = 5;
+        int ex = 6;
         Assert.assertSame("Не равны", ex, parser.getAmountTriangles());
 
     }
